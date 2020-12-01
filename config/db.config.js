09ea35b69b557292744da8647a730f04e54a1325
mptmadhushan <tharindumadhushan420@outlook.com>
2020-12-01 22:33:15 +0530

    aadded fixes

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "127.0.0.1",
-  USER: "root",
-  PASSWORD: "password",
-  DB: "photography",
+  HOST: "us-cdbr-east-02.cleardb.com",
+  USER: "bb17f03e941c10",
+  PASSWORD: "07e5d630",
+  DB: "heroku_9a1f4169864646e",
   dialect: "mysql",
   pool: {
     max: 5,
@@ -11,3 +11,16 @@ module.exports = {
     idle: 10000,
   },
 };
+// module.exports = {
+//   HOST: "127.0.0.1",
+//   USER: "root",
+//   PASSWORD: "password",
+//   DB: "photography",
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+// };
